64d1dfe459b215bf3741ca7e23f3d24637655180
nyocotluheker <sukipler47@gmail.com>
2026-03-05 08:32:42 +0700

    Create config.js

---
+++ settings/config.js
@@ -0,0 +1,45 @@
+import fs from "fs";
+import chalk from "chalk";
+
+/** info id **/
+global.owner = ["6285764889630","2@lid"];
+global.mode = true;
+
+/** apikey **/
+global.velynApiKey = "-";
+global.anaApiKey = "-";
+global.termaiApikey = "-";
+
+/** pairing Code Settings **/
+global.pairingPhoneNumber = "6285764889630"; 
+global.customPairingCode = "AAAAAAAA"; 
+
+/** settings panel **/
+global.egg = "15"; // Isi id egg
+global.nestid = "5"; // Isi id nest
+global.loc = "1"; // Isi id location
+global.domain = ""
+global.apikey = ""; // Isi api ptla
+global.capikey = ""; // Isi api ptlc
+
+/** nama bot **/
+global.namebotz = "------";
+global.packname = 'id.wwbs.net';
+global.nameown = "zaxsql";
+global.author = 'https://www.github.com/zax-universe';
+global.footer = "𝗍𝖾𝗅𝖾𝗀𝗋𝖺𝗆: @zaxsql | @wwbs_my_id";
+
+/** media **/
+global.YouTube = "https://www.instagram.com/@ambadev7";
+global.GitHub = "https://github.com/zax-universe";
+global.Telegram = "https://t.me/zaxsql";
+global.ChannelWA = "https://whatsapp.com/channel/0029VbCKPRs9mrGcwiG4gp14";
+
+/** message **/
+global.mess = {
+    group: "ngapain? khusus grup njrr",
+    admin: "ngapain? khusus admin njrr",
+    private: "ngapain? khusu private njirr",
+    owner: "apalah, bukan owner",
+    botadmin: "bot bukan admin"
+}
